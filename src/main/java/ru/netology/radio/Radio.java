5054c90f2529b1ea3c3f5cc0903a970f24f3bfef
Aleksey <Lesha55-90@mail.ru>
2023-09-26 21:37:42 +0600

    initial commit

---
+++ src/main/java/ru/netology/radio/Radio.java
@@ -0,0 +1,87 @@
+package ru.netology.radio;
+
+public class Radio {
+    // НАСТРОЙКА ЗВУКА!!!
+
+    public int currentVolume;          // текущий показатель громкости
+
+    public int getCurrentVolume() {    // запрос текущей громкости
+
+        return currentVolume;
+    }
+
+    public void setMaxVolume() {       // максимальная громкость
+        currentVolume = 100;
+    }
+
+    public void setMinVolume() {       // минимальная громкость
+
+        currentVolume = 0;
+    }
+
+
+    public void setCurrentVolume(int newCurrentVolume) {   // настройка условий (значений)
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 100) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+
+    public void reduceVolume() {        // уменьшение громкости
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void increaseVolume() {      // увеличение громкости
+        if (currentVolume < 100) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    // НАСТРОЙКА РАДИОСТАНЦИЙ!!!
+
+    public int currentStation;          // текущая радиостанция
+
+    public int getCurrentStation() {    // запрос текущей радиостанции
+        return currentStation;
+    }
+
+    public void setMaxStation() {       // максимальная радиостанция (значение)
+        currentStation = 9;
+    }
+
+    public void setMinStation() {       // минимальная радиостанция (значение)
+        currentStation = 0;
+    }
+
+    public void setCurrentStation(int newCurrentStation) {   // настройка условий (значений)
+        if (newCurrentStation > 9) {
+            return;
+        }
+        if (newCurrentStation < 0) {
+            return;
+        }
+        currentStation = newCurrentStation;
+    }
+
+    public void reduceStation() {        // переключения радиостанции (уменьшение на единицу)
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        } else {
+            setMaxStation();
+        }
+    }
+
+    public void increaceStation() {       // переключения радиостанции (увеличение на единицу)
+        if (currentStation < 9) {
+            currentStation = currentStation + 1;
+        } else {
+            setMinStation();
+        }
+    }
+}
---
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -0,0 +1,176 @@
+package ru.netology.radio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    // ТЕСТЫ   НА   ЗВУК
+
+    @Test
+    public void shouldVolume() {           // тест текущей громкости
+        Radio radio = new Radio();
+
+        radio.setCurrentVolume(50);
+
+        int expected = 50;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetMinVolume() {      // тест минимальной громкости
+        Radio radio = new Radio();
+        radio.setMinVolume();
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetMaxVolume() {       // тест максимальной громкости
+        Radio radio = new Radio();
+        radio.setMaxVolume();
+
+        int expected = 100;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetHiMaxVolume() {     // тест установки значения громксоти выше max значения
+        Radio radio = new Radio();
+        radio.setCurrentVolume(101);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetLowMinVolume() {    // тест установки значения громксоти ниже min значения
+        Radio radio = new Radio();
+        radio.setCurrentVolume(-1);
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReduceVolume() {       // тест понижения значения громкости (на единицу)
+        Radio radio = new Radio();
+        radio.setCurrentVolume(1);
+
+        radio.reduceVolume();
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaceVolume() {      // тест повышения значения громкости (на единицу)
+        Radio radio = new Radio();
+        radio.setCurrentVolume(99);
+
+        radio.increaseVolume();
+
+        int expected = 100;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    // ТЕСТЫ   РАДИОСТАНЦИИ
+
+    @Test
+    public void shouldStation() {             // тест текущей радиостанции
+        Radio radio = new Radio();
+
+        radio.setCurrentStation(5);
+
+        int expected = 5;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetMaxStation() {       // тест максимальной радиостанции
+        Radio radio = new Radio();
+        radio.setMaxStation();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetMinStation() {       // тест минимальной радиостанции
+        Radio radio = new Radio();
+        radio.setMinStation();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetHiMaxStation() {     // тест ввода значения радиостанции выше max значения
+        Radio radio = new Radio();
+        radio.setCurrentStation(10);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetLowMinStation() {     // тест ввода значения радиостанции ниже min значения
+        Radio radio = new Radio();
+        radio.setCurrentStation(-1);
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReduceStation() {       // тест переключения радиостанции (уменьшение на единицу)
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+
+        radio.reduceStation();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaceStation() {      // тест переключения радиостанции (увеличение на единицу)
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+
+        radio.increaceStation();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
